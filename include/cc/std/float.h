b72a4e8f19c6f9598e6f7fcf1fc0bfd2ac8867d3
Cuong Dang <cuongd@pm.me>
2025-12-05 22:58:01 -0600

    std/float: Per ISO/IEC 9899:1999

--- include/cc/std/float.h
+++ include/cc/std/float.h
@@ -6,15 +6,26 @@
 #define FLT_RADIX 2
 #define FLT_ROUNDS 1
 
+/* float */
+#define FLT_DIG 6
+#define FLT_EPSILON 0X1P-23F
+#define FLT_MANT_DIG 24
+#define FLT_MAX 0X1.fffffeP127F
+#define FLT_MAX_10_EXP (+38)
+#define FLT_MAX_EXP (+128)
+#define FLT_MIN 0X1P-126F
+#define FLT_MIN_10_EXP (-37)
+#define FLT_MIN_EXP (-125)
+
 /* double */
 #define DBL_DIG 15
-#define DBL_EPSILON 2.2204460492503131E-16
+#define DBL_EPSILON 0X1P-52
 #define DBL_MANT_DIG 53
-#define DBL_MAX 1.7976931348623157E+308
-#define DBL_MAX_10_EXP _Dbl.max10e
-#define DBL_MAX_EXP _Dbl.maxe
-#define DBL_MIN _Dbl.min.d
-#define DBL_MIN_10_EXP _Dbl.min10e
-#define DBL_MIN_EXP _Dbl.mine
+#define DBL_MAX 0X1.fffffffffffffP1023
+#define DBL_MAX_10_EXP (+308)
+#define DBL_MAX_EXP (+1024)
+#define DBL_MIN 0X1P-1022
+#define DBL_MIN_10_EXP (-307)
+#define DBL_MIN_EXP (-1021)
 
 #endif
